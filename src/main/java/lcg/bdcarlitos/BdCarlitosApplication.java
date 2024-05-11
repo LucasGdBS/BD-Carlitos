afb7d0a1683eb49e4222cf829c6898dd6613dd48
Lucas-PC <lgbs@cesar.school>
2024-05-11 16:26:36 -0300

    commit inicial do projeto

---
+++ src/main/java/lcg/bdcarlitos/BdCarlitosApplication.java
@@ -0,0 +1,13 @@
+package lcg.bdcarlitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdCarlitosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdCarlitosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lcg/bdcarlitos/BdCarlitosApplicationTests.java
@@ -0,0 +1,13 @@
+package lcg.bdcarlitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdCarlitosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
