01f4f9bdca386e6c5c958c4f599d9df8748a6158
Lucas-PC <lgbs@cesar.school>
2024-05-14 14:54:53 -0300

    adding try catch to getAllFuncionarios

--- src/main/java/lcg/bdcarlitos/controllers/FuncionarioController.java
+++ src/main/java/lcg/bdcarlitos/controllers/FuncionarioController.java
@@ -1,14 +1,13 @@
 package lcg.bdcarlitos.controllers;
 
 
-import lcg.bdcarlitos.entities.Funcionario;
 import lcg.bdcarlitos.services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/funcionarios")
@@ -18,7 +17,13 @@ public class FuncionarioController {
     private FuncionarioService funcionarioService;
 
     @GetMapping("/")
-    public List<Funcionario> getAllFuncionarios(){
-        return funcionarioService.getAll();
+    public ResponseEntity<?> getAllFuncionarios(){
+        try{
+            return new ResponseEntity<>(funcionarioService.getAll(), HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity<>("Erro ao buscar Clientes " + e.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
